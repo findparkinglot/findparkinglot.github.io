6c4ae9568bf989fcd7c023e75b08879dcbf97934
ray01 <ray01.lin@gigabyte.com>
2024-05-06 18:29:31 +0800

    room

--- src/components/MapBox.vue
+++ src/components/MapBox.vue
@@ -121,14 +121,14 @@ const setMaker = () => {
             // These options control the ending camera position: centered at
             // the target, at zoom level 9, and north up.
             center: marker.geometry.coordinates,
-            zoom: 18,
+            zoom: 17,
             bearing: 0,
 
             // These options control the flight curve, making it move
             // slowly and zoom out almost completely before starting
             // to pan.
-            speed: 1, // make the flying slow
-            curve: 1, // change the speed at which it zooms out
+            speed: 1.5, // make the flying slow
+            curve: 1.5, // change the speed at which it zooms out
 
             // This can be any easing function: it takes a number between
             // 0 and 1 and returns another number between 0 and 1.
